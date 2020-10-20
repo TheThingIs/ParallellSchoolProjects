19ccb1795ab96557f6678c975bd44d6be8c08838
PettHub <aliver.ondersson@gmail.com>
2020-10-20 10:19:54 +0200

    fixade färger

--- src/View/SchemaWorkshift.java
+++ src/View/SchemaWorkshift.java
@@ -5,8 +5,12 @@ import Model.WeekHandler;
 import Model.WorkShift;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.paint.Color;
 
 import java.util.Date;
@@ -41,30 +45,8 @@ public class SchemaWorkshift extends AnchorPane {
             this.timeBar.setStyle("-fx-background-color: #BBBBBB");
             this.name.setText("Ledig!");
         } else {
-            StringBuilder red = new StringBuilder(), blue = new StringBuilder(), green = new StringBuilder();
-            if (color.getRed() < 1 / 16.0)
-                red.append("0");
-            if (color.getRed() == 0)
-                red.append("0");
-            red.append(Integer.toHexString((int) (color.getRed() * 255)));
-            if (color.getGreen() < 1 / 16.0)
-                green.append("0");
-            if (color.getGreen() == 0)
-                green.append("0");
-            green.append(Integer.toHexString((int) (color.getGreen() * 255)));
-            if (color.getBlue() < 1 / 16.0) {
-                blue.append("0");
-            }
-            if (color.getBlue() == 0) {
-                blue.append("0");
-            }
-            blue.append(Integer.toHexString((int) (color.getBlue() * 255)));
-            /*
-            System.out.println(Integer.toHexString((int)(color.getRed()*255)));
-            System.out.println(Integer.toHexString((int)(color.getGreen()*255)));
-            System.out.println(Integer.toHexString((int)(color.getBlue()*255)));
-            */
-            this.timeBar.setStyle("-fx-background-color: #" + red.toString() + green.toString() + blue.toString());
+            BackgroundFill tmp = new BackgroundFill(color, new CornerRadii(0), new Insets(0));
+            this.timeBar.setBackground(new Background(tmp));
             this.name.setText(workShift.getEmployee().getPersonalId());
         }
     }
