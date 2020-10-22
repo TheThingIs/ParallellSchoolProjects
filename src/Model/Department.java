5494faa6df65184c95aabad9af5b7a66e9f48fbc
Ronserro <33457400+Ronserro@users.noreply.github.com>
2020-10-22 13:22:29 +0200

    Tagit bort oanvända metoder

--- src/Model/Department.java
+++ src/Model/Department.java
@@ -218,19 +218,6 @@ public class Department implements Observable {
         return allShifts.size();
     }
 
-    /**
-     * Checks if all work shift of the department are manned
-     *
-     * @return true if all work shifts are manned, else false
-     */
-    public boolean isAllShiftsFilled() {
-        for (WorkShift ws : allShifts) {
-            if (!ws.isOccupied())
-                return false;
-        }
-        return true;
-    }
-
     public void setMinPersonsOnShift(int minPersonsOnShift) {
         this.minPersonsOnShift = minPersonsOnShift;
     }
